aaca4ea58f56e262b6858117d5ed85658f1a56ef
w01ki3 <59314667+w01ki3@users.noreply.github.com>
2022-01-07 17:56:51 +0300

    Create showPage.blade.php

---
+++ resources/views/site/showPage.blade.php
@@ -0,0 +1,14 @@
+@php 
+$lang = Lang::getLocale()
+@endphp
+
+<div class="container">
+  <div class="row">
+    <div class="col-12 col-lg-12">
+      {!! $page->content($lang) !!}
+    </div>
+  </div>
+<div>
+
+
+                
